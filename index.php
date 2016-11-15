075b7a8d3f7b080589ea45ba3c28d6de3ecea1cc
THEozmic <michaelozoemena2@gmail.com>
2016-11-15 18:17:43 +0100

    Genesis

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 <html>
 <head>
 	<title>Qristal Devs</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
 	<div>
 		<div class="chat-board"></div>
-		<div class="chat-input"></div>
+		<div class="chat-input">
+		<form>
+			<input type="text" placeholder="Type your message here..." name="message">
+		</form>
+
+		</div>
 	</div>
 
 </body>
