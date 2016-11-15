96ddad90c1adcc2fcd910c30bcad1be05c6b82ca
THEozmic <michaelozoemena2@gmail.com>
2016-11-15 18:13:12 +0100

    Genesis

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Qristal Devs</title>
+</head>
+<body>
+
+	<div>
+		<div class="chat-board"></div>
+		<div class="chat-input"></div>
+	</div>
+
+</body>
+</html>
